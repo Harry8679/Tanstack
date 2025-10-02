db1ef6cfb5fceafc93c96ef247a7db0ab7242679
Harry7986 <emarh.harry.code@gmail.com>
2025-10-02 18:37:40 +0200

    App 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import './App.css';
 import axios from 'axios';
+import type { Todo } from './types';
 
 const getToDos = async () => {
     const response = await axios('https://jsonplaceholder.typicode.com/todos');
@@ -17,8 +18,8 @@ function App() {
     <h1>{isSuccess && <p>Data fetched successfully</p>}</h1>
     <ul>
       {isSuccess && 
-        data.map((todo: { id: number, title: string }) => (
-          <li key={todo.id}>{todo.title}</li>
+        data.map((todo: Todo) => (
+          <li key={todo.userId}>{todo.title}</li>
         ))}
       {error && <p>{error.message}</p>}
     </ul>
